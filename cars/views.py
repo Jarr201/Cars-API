54c7590e6e2fe7aae576f31e1cce7aecd06c7323
Jalen Arrington <jmarrington178@yahoo.com>
2022-08-24 12:12:27 -0400

    Added more sections tot he cars_list function on the views.py file

--- cars/views.py
+++ cars/views.py
@@ -5,9 +5,15 @@ from .models import Car
 
 @api_view(['GET'])
 def cars_list(request):
-    cars = Car.objects.all()
-
-    serializer = CarSerializer(cars, many=True)
-
-
-    return Response(serializer.data)
+    
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializer = CarSerializer(cars, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
